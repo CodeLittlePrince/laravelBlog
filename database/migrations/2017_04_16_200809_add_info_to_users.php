f9c79f696e0ae4590e7e3a8edb76159380dbcffa
CodeLittlePrince <1006312908@qq.com>
2017-04-17 04:37:42 +0800

    migrate

--- database/migrations/2017_04_16_200809_add_info_to_users.php
+++ database/migrations/2017_04_16_200809_add_info_to_users.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class AddInfoToUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name', '20')->unique();
-            $table->integer('level');
-            $table->string('avatar');
-            $table->timestamps();
+        Schema::table('users', function($table){
+            $table->string('email')->unique();
+            $table->string('password', 60);
+            $table->rememberToken();
         });
     }
 
@@ -29,6 +27,8 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function($table){
+            $table->drop('users');
+        });
     }
 }
