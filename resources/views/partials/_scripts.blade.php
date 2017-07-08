ac658ddb1cbbf6630ded2d435bb0ba8a15da2d14
CodeLittlePrince <1006312908@qq.com>
2017-07-09 04:21:46 +0800

    scrolltofixed 改成cdn的

--- resources/views/partials/_scripts.blade.php
+++ resources/views/partials/_scripts.blade.php
@@ -3,7 +3,7 @@
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 {{-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script> --}}
 <script src="https://cdn.staticfile.org/highlight.js/9.10.0/highlight.min.js"></script>
-<script src="{{ asset('vendors/scrolltofixed/scrolltofixed.min.js') }}"></script>
+<script src="https://cdn.staticfile.org/ScrollToFixed/1.0.8/jquery-scrolltofixed-min.js"></script>
 <script type="text/javascript">
 	hljs.initHighlightingOnLoad();
 	$(function() {
