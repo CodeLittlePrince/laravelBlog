c39d4aa72b9491b48fa4635f59a0e74c92bb3e1a
CodeLittlePrince <1006312908@qq.com>
2017-07-10 23:18:06 +0800

    优化编辑器样式

--- resources/assets/js/post/create.js
+++ resources/assets/js/post/create.js
@@ -2,5 +2,23 @@ var Simditor = require('simditor');
 var editor = new Simditor({
     textarea: $('#content'),
     //optional options
-    defaultImage: 'http://or1edgicq.bkt.clouddn.com/fate.png?imageView2/1/w/100/h/100'
+    defaultImage: 'http://or1edgicq.bkt.clouddn.com/fate.png?imageView2/1/w/100/h/100',
+    toolbar: [
+		'title',
+		'bold',
+		'italic',
+		'underline',
+		'strikethrough',
+		'fontScale',
+		'color',
+		'ol',
+		'ul',
+		'blockquote',
+		'code',
+		'link',
+		'image',
+		'indent',
+		'outdent',
+		'alignment'
+	]
 });
