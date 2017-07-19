7b871f177e75af80398323f7a31eed7d84b360b5
CodeLittlePrince <1006312908@qq.com>
2017-07-19 11:37:52 +0800

    加友情链接和备案

--- resources/views/partials/_foot.blade.php
+++ resources/views/partials/_foot.blade.php
@@ -1,2 +1,4 @@
 <hr/>
-<p class="text-center">骡子窝 - All Right Preserved</p>
+<p class="text-center">网站友链：<a class="text-muted" href="http://www.yodfz.com/" target="_blank">写代码的熊猫</a></p>
+<p class="text-center">Copyright©2017 骡子窝 luoziwo.cn 版权所有 - All Right Preserved</p>
+<p class="text-center">浙ICP备17030485号-1</p>
